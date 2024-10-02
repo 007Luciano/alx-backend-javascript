f8c8d99e78f526df1e8b808cfc6f13d418fb38c7
007Luciano <lucky.h.fatch@gmail.com>
2024-10-02 02:54:17 +0000

    full server

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,19 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false,
+});
+
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.question('', (name) => {
+  console.log(`Your name is: ${name.trim()}`);
+
+  if (!process.stdin.isTTY) {
+    console.log('This important software is now closing');
+  }
+
+  rl.close();
+});
